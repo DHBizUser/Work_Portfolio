b6eb31116648032dbf18898ad5c449b74dd284b6
DHBizUser <davidh804408@gmail.com>
2025-06-26 22:53:41 -0400

    fix author

---
+++ helper-lisp.el
@@ -0,0 +1,33 @@
+;; Thu Jun 26 19:52:19 2025 -- chuggpup --
+;; S-<insert>   ~/setxkbmap-setter
+;; toggles the keyboard
+;; this is the custom layout on CapsLock and the bottom row
+;; ------                                             ------
+;; Meta                                               Return
+;; ------            {Programmer Dvorak}              ------
+;; Shift                                               Shift
+;; ---------------------------------------------------------
+;; Hyper - Super - Ctrl - Space - Ctrl - Meta - Menu - Hyper
+;; ---------------------------------------------------------
+
+;; radio:
+;; H-r 1  ~/🚀synthwavecityFM-radio
+;; H-r 2  ~/🍄psybient-radio
+;; H-r 3  ~/🖭NewRetroWave
+
+
+;; C-x 5 2    opens another emacs frame, or C-<F1> using the desktop
+;; s-<end>  to save-buffers-kill-emacs
+
+;; This buffer is for text that is not saved, and for Lisp evaluation.
+;; To create a file, visit it with C-x C-f and enter text in its buffer.
+
+
+
+(mkdir "~/Projects/Work_Portfolio")
+
+(cd "~/Projects/Work_Portfolio")
+
+(pwd)
+
+(shell-command "git clone ssh://githubDHBizUserWork_Portfolio.git")
